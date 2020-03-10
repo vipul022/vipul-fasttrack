7b3cab763e0a36d89e8899be5e489c799327a88e
vipul022 <vipul.seth022@gmail.com>
2020-03-10 15:02:00 +1100

    next version of gem

--- lib/vipuls_fast_track.rb
+++ lib/vipuls_fast_track.rb
@@ -1,6 +1,3 @@
 require_relative "vipuls_fast_track/version"
 
-module VipulsFastTrack
-  class Error < StandardError; end
-  "hello"
-end
+puts "hello"
--- lib/vipuls_fast_track/version.rb
+++ lib/vipuls_fast_track/version.rb
@@ -1,3 +1,3 @@
 module VipulsFastTrack
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
