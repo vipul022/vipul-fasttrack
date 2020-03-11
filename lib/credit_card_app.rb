d90c41778fad18e5e0160f606c476d241ccc9092
vipul022 <vipul.seth022@gmail.com>
2020-03-11 15:39:06 +1100

    created apply credit card,update credit card functionalities

--- lib/credit_card_app.rb
+++ lib/credit_card_app.rb
@@ -35,51 +35,146 @@ def read_customers_csv
 
 def check_details
   customers = read_customers_csv
+   puts "please enter your NAB ID"
+    customer_input = gets.chomp
+   
+   customers.each do |customer|
+     customer = customer.to_hash
+      # p customer['nab_id']
+      if customer_input == customer['nab_id']
+      # puts 'hello'
+    #  puts "nab id: #{customer['nab_id']}"
+    puts "customer name: #{customer['customer_name']}"
+    puts "monthly income: $ #{customer['monthly_income']}"
+    puts "monthly expenses: $ #{customer['monthly_expenses']}"
+      else
+        puts "invalid NAB ID"
+      end
+      end
+     end 
+    
+      def new_customer
+       puts 'what is your full name?'
+        name = gets.chomp
+         puts 'whats is your monthly income?'
+          income = gets.chomp.to_i
+           puts 'what are your monthly expenses?'
+        expenses = gets.chomp.to_i
+         new_clint = [name,income,expenses]
+           monthly_surplus = income - expenses
+          #  p monthly_surplus
+              if monthly_surplus > 2000
+                puts
+                puts 'Congratulations your application has been approved'
+                puts '-' *40
+              else 
+                 puts 
+                puts 'Sorry!!!! your application has been declined'
+                puts '-' *40
+              end 
+        CSV.open('customers.csv' , 'a') do |csv|
+        csv  << new_clint
+       end
+      end
+         
+
+def find_customer(customers, id_input)
   customers.each do |customer|
-    customer = customer.to_hash
-    p customer
-    # "Username: #{user["username"]} "
-    puts "nab id: #{customer['nab_id']}"
-  end
+    customer_hash = customer.to_hash
+    if id_input == customer["nab_id"] 
+      return customer_hash
+    end 
+  end 
 end
-# def apply_credit_card
-#   puts 'Are you a current NAB customer? (reply yes/no) '
 
-# end
-# puts 'Are you a current NAB customer?'
-# puts 'Select 1. for yes'
-# puts 'select 2. for no'
-# customer_input = gets.chomp.to_i
-# if customer_input = 1
-#   puts '*' * 30
-#   puts 'Please enter your name'
-#   name = gets.chomp
+def apply_credit_card
+  customers = read_customers_csv
+  puts "Welcome to NAB"
+  puts "-" * 40
+  puts 'Are you a current NAB customer? (reply yes/no)' 
+  customer_input = gets.chomp
+  if customer_input == 'yes'
+    puts 'please enter your NAB ID'
+    id_input = gets.chomp
+    customer = find_customer(customers, id_input)
+    monthly_surplus = customer['monthly_income'].to_i - customer['monthly_expenses'].to_i
+    if (id_input == (customer['nab_id'])) && (monthly_surplus > 2000)
+      puts
+      puts 'Congratulations your application has been approved'
 
-#   customers.each do |customer|
-#     if name == customer[:customer_name]
+      puts
+    else
+      puts
+      puts 'sorry! your application has been declined'
+      puts
+    end
+      elsif customer_input == 'no'
+         new_customer 
+        
+    end  
+end 
+        
+ def update_credit_card
+      customers = read_customers_csv
+    puts 'Do you want to update your monthly income? (reply yes/no)'
+    puts '-' *40
+    customer_input = gets.chomp
+     if customer_input =='yes'
+    puts 'Please enter your NAB ID'
+      id_input = gets.chomp
+    puts 'Please enter your updated monthly income' 
+      new_income = gets.chomp
+        # customer = find_customer(customers, id_input) 
+        customers.each do |customer|
+          if customer["nab_id"] == id_input
+            customer["monthly_income"] = new_income
+          end
+        end  
+  puts      
+  puts 'Your monthly income has been successfully updated.'      
+            CSV.open('customers.csv', 'w') do |csv|
+              csv << ['nab_id','customer_name','monthly_income','monthly_expenses']
+              customers.each do |customer|
+                csv << customer.to_h.values
+              end
 
-#     end
-#   end
-# end
-while
+            end
+            else  
+               exit
+          end
+ end       
+              
+
+  # def cancel_credit_card
+  #      customers = read_customers_csv
+  #       "Do you want to cancel your credit card? (reply yes/no)"
+  #         customer_input = gets.chomp
+  #           if customer_input == 'yes'
+  #               puts " Please enter your NAB id"
+  #                id_input = gets.chomp 
+                  
+
+
+                   
+
+
+  end      
+    
+      
+
+while true
   customer_input = menu
   case customer_input
   when 1
     apply_credit_card
   when 2
     check_details
+  when 3
+    update_credit_card
+  when 4
+    cancel_credit_card 
   end
 end
-# while true
-#   user_input = menu
-#   users = read_users_csv
-#   case user_input
-#   when 1
-#   view_contacts(users)
-#   when 2
-#   add_contacts(users)
-#   when 3
-#   puts remove_contacts(users)
-#   else exit
-#   end
-# end
+    
+
+
